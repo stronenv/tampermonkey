54122bd2f93d37c299444cb0c1ae5dc3426b8271
Vegard Stronen <70795294+stronenv@users.noreply.github.com>
2023-04-04 19:57:20 +0200

    Replaced if statements with regex

--- front2ZendeskSearch.user.js
+++ front2ZendeskSearch.user.js
@@ -1,28 +1,29 @@
 // ==UserScript==
 // @name         Replace Front links with Zendesk search query
 // @description  Will try to replace all front reefernces with a Zendesk search query.
-// @version      0.3
+// @version      0.5
 // @author       stronenv
 // @match        https://*.wordpress.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
+// Grab all A tags
 var allLinks = document.getElementsByTagName('a');
 
+// Cycle through all A tags
 for (var i = 0; i < allLinks.length; i++) {
-    let thisLink = allLinks[i];
-    let thisLinkUrl = thisLink.href.replace("https://href.li/?", "");
 
-    if(thisLinkUrl.includes("https://app.frontapp.com")) {
+    // Check if the href includes a frontapp.com link
+    if(allLinks[i].href.includes("https://app.frontapp.com")) {
 
-        let url = new URL(thisLinkUrl)
-        let pathname = url.pathname
-        let front_search_q = pathname.replace("/open/", "")
-        if( front_search_q.includes("msg_") || front_search_q.includes("cnv_") ) {
-            allLinks[i].setAttribute("href", "https://href.li/?https://woothemes.zendesk.com/agent/search/1?copy&type=ticket&q=" + front_search_q);
+
+        // Look for Front IDs
+        let matches = allLinks[i].href.match(/\/open\/([a-z]{3}_.{7})/m);
+
+        // If we have Front IDs, update the href attribute
+        if (typeof matches[1] != "undefined") {
+            allLinks[i].setAttribute("href", "https://href.li/?https://woothemes.zendesk.com/agent/search/1?copy&type=ticket&q=" + matches[1]);
         }
-
     }
-
 }
