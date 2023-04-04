59e4286a0779ca2b61f05686dd039eb23e29570a
Vegard Stronen <70795294+stronenv@users.noreply.github.com>
2023-04-04 19:59:50 +0200

    Version bump and clean up

--- front2ZendeskSearch.user.js
+++ front2ZendeskSearch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replace Front links with Zendesk search query
 // @description  Will try to replace all front reefernces with a Zendesk search query.
-// @version      0.5
+// @version      0.6
 // @author       stronenv
 // @match        https://*.wordpress.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -22,7 +22,9 @@ for (var i = 0; i < allLinks.length; i++) {
 
         // If we have Front IDs, update the href attribute
         if (typeof matches[1] != "undefined") {
+
             allLinks[i].setAttribute("href", "https://href.li/?https://woothemes.zendesk.com/agent/search/1?copy&type=ticket&q=" + matches[1]);
+
         }
     }
 }
