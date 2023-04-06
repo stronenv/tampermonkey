c1b7a57041e7581b71b86e43cd5b84d0f9c5359d
Vegard Stronen <70795294+stronenv@users.noreply.github.com>
2023-04-06 10:03:42 +0200

    Update the check for array values in matches
    The previous check failed if the array, matches, didn't exist at all (for example, if there were front links not linking to a message or conversation). The if statement has been updated to check for an array and the length instead of a specific key.

--- front2ZendeskSearch.user.js
+++ front2ZendeskSearch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replace Front links with Zendesk search query
 // @description  Will try to replace all front reefernces with a Zendesk search query.
-// @version      0.6
+// @version      0.7
 // @author       stronenv
 // @match        https://*.wordpress.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -21,7 +21,7 @@ for (var i = 0; i < allLinks.length; i++) {
         let matches = allLinks[i].href.match(/\/open\/([a-z]{3}_.{7})/m);
 
         // If we have Front IDs, update the href attribute
-        if (typeof matches[1] != "undefined") {
+        if (Array.isArray(matches) && matches.length >= 1) {
 
             allLinks[i].setAttribute("href", "https://href.li/?https://woothemes.zendesk.com/agent/search/1?copy&type=ticket&q=" + matches[1]);
 
